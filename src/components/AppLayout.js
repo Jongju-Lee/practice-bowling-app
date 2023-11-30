f35451ae461d274dcfec5175c013cc8ea06e7063
jongju-Lee <jeje7@naver.com>
2023-11-30 22:22:33 +0900

    My Club버튼 구현 완료

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -5,7 +5,7 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Container } from "@mui/material";
+import { Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 import styled from "styled-components";
@@ -20,64 +20,62 @@ const AppLayout = ({ children, isLoggedIn }) => {
   };
 
   return (
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          flexGrow: 1,
-          overflowY: "auto",
-          height: "100vh",
-        }}
-      >
-        <AppBar
-          color="transparent"
-          position="fixed"
-          sx={{ top: 0, zIndex: 1000, width: "100%" }}
+    <Grid container>
+      <Grid item xs={12}>
+        <Box
+          sx={{
+            flexGrow: 1,
+          }}
         >
-          <Toolbar>
-            <IconButton
-              size="string"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              onClick={navigateHome}
-              sx={{
-                flexGrow: 1,
-                display: "block",
-                position: "absolute",
-                left: "50%",
-                transform: "translateX(-50%)",
-                cursor: "pointer",
-                fontFamily: "Nova Square, sans-serif",
-              }}
-            >
-              AWESOME
-            </Typography>
-            {isLoggedIn ? (
-              <MyButton
-                text="MY CLUB"
-                onClick={() => navigate("/myclub")}
-                sx={xsValue}
-              />
-            ) : (
-              <MyButton
-                text="Join"
-                onClick={() => navigate("/signup")}
-                sx={xsValue}
-              />
-            )}
-          </Toolbar>
-        </AppBar>
-        <div style={{ marginTop: "5rem" }}>{children}</div>
-      </Box>
-    </Container>
+          <AppBar color="transparent">
+            <Toolbar>
+              <IconButton
+                size="string"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                onClick={navigateHome}
+                sx={{
+                  flexGrow: 1,
+                  display: "block",
+                  position: "absolute",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  cursor: "pointer",
+                  fontFamily: "Nova Square, sans-serif",
+                }}
+              >
+                AWESOME
+              </Typography>
+              {isLoggedIn ? (
+                <MyButton
+                  text="MY CLUB"
+                  onClick={() => navigate("/myclub")}
+                  sx={xsValue}
+                />
+              ) : (
+                <MyButton
+                  text="Join"
+                  onClick={() => navigate("/signup")}
+                  sx={xsValue}
+                />
+              )}
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <div style={{ marginTop: "4rem" }}>{children}</div>
+      </Grid>
+    </Grid>
   );
 };
 
